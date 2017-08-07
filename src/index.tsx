59709c79d524448c49dac92663fb8e50b3bc46b8
Nukr <nukrs.w@gmail.com>
2017-08-07 14:09:33 +0800

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { BasicExample } from './router';
+
+ReactDOM.render(
+  <BasicExample />,
+  document.getElementById('example')
+);
